47becaa864b434d2a0dcaefe7c651addf9794093
josehfortes <jose@seja.dev>
2022-08-01 00:54:58 -0300

    started kit sejadev

---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+function HomePage () {
+  return (
+    <div>
+      <h1>BitcoinDev</h1>
+      <p>Acompanhe o gráfico da principal cryptomoeda da atualidade.</p>
+    </div>
+  )
+}
+
+export default HomePage
